09cb5b8a4d2c8360216e9d587be1db543eaea243
Craig Isesele <cmoisesele@live.com>
2018-06-25 09:19:03 -0400

    Implemented function
    Write fibbonaci recursively

--- Reddit/general.py
+++ Reddit/general.py
@@ -1,76 +1,20 @@
-def most_frequent_integer_in_array(array):
-    # Find the most frequent integer in an array
-    count = 0
+def fibonacci_recursive(n):
+    if n is 0:
+        return 0
+    elif n is 1 or n is 2:
+        return 1
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+def fibonacci_iterative(n):
 
-def find_pairs(array):
-    # Find pairs in an integer array whose sum is equal to 10 
-    # (bonus: do it in linear time)
-    pass
+    return n
 
-def check_if_array_is_rotated(array1, array2):
-    # Given 2 integer arrays, 
-    # determine of the 2nd array is a rotated version of the 1st array. 
-    # Ex. Original Array A={1,2,3,5,6,7,8} Rotated Array B={5,6,7,8,1,2,3}
-    pass
+def fibonacci(n):
+    result0 = fibonacci_recursive(n)
+    result1 = fibonacci_iterative(n)
+    print(result0)
+    print(result1)
+fibonacci(7)
 
-def fibonacci_iterative():
-    pass
-
-def fibonacci_recursive():
-    # bonus - use dynamic programming
-    pass
-
-def find_unique_element(array):
-    # Find the only element in an array that only occurs once.
-    pass
-
-def find_common_elements(array):
-    # Find the common elements of 2 int arrays
-    pass
-
-def binary_search_sorted_array():
-    # Implement binary search of a sorted array of integers
-    pass
-
-def binary_search_recursive_array():
-    # Implement binary search in a rotated array (ex. {5,6,7,8,1,2,3})
-    pass
-
-def find_first_prime_numbers():
-    # Use dynamic programming to find the first X prime numbers
-    pass
-
-def convert_int_to_binary(number):
-    # Write a function that prints out the binary form of an int
-    print(number)
-    pass
-
-def parse_int():
-    pass
-
-def square_root():
-    pass
-
-def exponent_function():
-    # (bonus: now try in log(n) time)
-    pass
-
-def multiply(i, j):
-    # Write a multiply function that multiples 2 integers without using *
-    pass
-
-def rand5():
-    # Given a function rand5() that returns a random int between 0 and 5
-    pass
-
-def rand7():
-    pass
-
-def justify(text, max_width):
-    # Given a single-line text String and a maximum width value, 
-    # write the function 'String justify(String text, int maxWidth)' 
-    # that formats the input text using full-justification, 
-    # i.e., extra spaces on each line are equally distributed between the words; 
-    # the first word on each line is flushed left and the last word on each line 
-    # is flushed right
-    pass
+def multiply(i,j):
+    if (i is 0) or (j is 0):
+        return 0
