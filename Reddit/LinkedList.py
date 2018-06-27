ef3507af56e0a2099cb71449ca86f744c4cbb810
Craig Isesele <cmoisesele@live.com>
2018-06-27 14:03:07 -0400

    Implemented function
    Inserting data into linked list

--- Reddit/LinkedList.py
+++ Reddit/LinkedList.py
@@ -10,34 +10,43 @@ class LinkedList():
     def __init__(self):
         self.head = None
         self.curr = None
-        self.last = None
-    def insert(self, node, index=0):
+        # self.last = None
+    def insert(self, value, index=0):
+        node = Node(value)
         if self.head is None:
             self.head = node
-            self.curr = self.head
         else:
-            self.curr.next = node
-            self.curr = self.curr.next
-            node.next = self.last
-        # self.head.next = self.last
+            if index is 0:
+                node.next = self.head
+                self.head = node
+            else:
+                self.curr = self.traverse(index)
+                node.next = self.curr.next
+                self.curr.next = node
+    # def swap(self,new_node, old_node):
+
+
     def traverse(self, index):
         node = self.head
         while node is not None and index is not 0:
             index-=1
             node = node.next
         return node
-        
-
     def __str__(self):
         nodestr = ""
         node = self.head
+        count = 1
         while node is not None:
-            nodestr+=node
+            nodestr+=" " + (str(count)) + "."
+            nodestr+=node.__str__()
+            count+=1
             node = node.next
+        
         return nodestr
 
 list = LinkedList()
 list.insert(30)
 list.insert(20)
+list.insert(40,1)
 print(list)
     
